c1d3e5911a77d5e96146f8f17fdcf8e0bcc72e2f
yungs17 <97180762+yungs17@users.noreply.github.com>
2023-02-28 19:05:15 +0900

    s

--- contracts/core/ConditionResolver.sol
+++ contracts/core/ConditionResolver.sol
@@ -2,7 +2,55 @@
 pragma solidity ^0.8.0;
 
 contract ConditionResolver {
+
+    // method function selector => target contract view function selectors
+    mapping(bytes4 => bytes4[]) private explicitSelectors;
+
     constructor(){
+        //enum => uint8
+        explicitSelectors[bytes4(keccak256("checkBalanceOf(address,address,uint256,uint8)"))] = [IERC20.balanceOf.selector];
+    }
+
+    enum Compare
+    {
+        EQUAL,
+        NOT_EQUAL,
+        GREATER,
+        GREATER_EQUAL
+    }
+
+    function compareUint(uint a, uint b, Compare compare) returns (bool){
+        if (compare == Compare.EQUAL) {
+            return a == b;
+        }
+        else if (compare == Compare.NOT_EQUAL) {
+            return a != b;
+        }
+        else if (compare == Compare.GREATER) {
+            return a > b;
+        }
+        else {
+            return a >= b;
+        }
+    }
+
+    function getExplicitSelectors(bytes4 methodSelector) view returns(bytes4[]){
+        return explicitSelectors[methodSelector];
+    }
+
+    function checkBalanceOf(address targetDERC20Contract, address targetAddress, uint256 criterion, Compare compare) view returns (bool){
+        //TODO: handle exceptions
+        //require targetDERC20Contract.supportsInterface(IDERC20);
+
+        (bool success, uint256 balance) = targetDERC20Contract.staticcall(abi.encodeWithSelector(IERC20.balanceOf.selector, targetAddress));
+
+        if (compareUint(balance, criterion, compare)) {
+            return true;
+        }
+        else return false;
 
     }
+
+
 }
+
